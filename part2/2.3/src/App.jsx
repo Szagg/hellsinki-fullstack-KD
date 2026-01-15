86f85d683784f015e7318a9b62e6ca7fc33b1ba6
Karol Domański <147516716+Szagg@users.noreply.github.com>
2026-01-15 13:24:08 +0100

    commit

--- part2/2.3/src/App.jsx
+++ part2/2.3/src/App.jsx
@@ -32,53 +32,29 @@ const Course = ({ course }) => (
 )
 
 const App = () => {
-  const courses = [
-    {
-      id: 1,
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3,
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Node.js',
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ]
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
 
-  return (
-    <div>
-      {courses.map((c) => (
-        <Course key={c.id} course={c} />
-      ))}
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
