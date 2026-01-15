5c7b86be9347100f63dbb2685ae240e180d8e484
Karol Domański <147516716+Szagg@users.noreply.github.com>
2026-01-15 13:07:33 +0100

    update

--- part2/2.1/src/App.jsx
+++ part2/2.1/src/App.jsx
@@ -1,3 +1,36 @@
+const Header = ({ name }) => <h1>{name}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((p) => (
+      <Part key={p.id} part={p} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const sum = parts.reduce((s, p) => s + p.exercises, 0)
+  return (
+    <p>
+      <strong>total of {sum} exercises</strong>
+    </p>
+  )
+}
+
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
 const App = () => {
   const course = {
     id: 1,
@@ -6,19 +39,19 @@ const App = () => {
       {
         name: 'Fundamentals of React',
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
         name: 'State of a component',
         exercises: 14,
-        id: 3
-      }
-    ]
+        id: 3,
+      },
+    ],
   }
 
   return <Course course={course} />
