f730310375d7d15e3cd5470685e66ebf7eff9d37
Karol Domański <147516716+Szagg@users.noreply.github.com>
2026-01-15 13:11:19 +0100

    update

--- part2/2.1/src/App.jsx
+++ part2/2.1/src/App.jsx
@@ -14,14 +14,7 @@ const Content = ({ parts }) => (
   </div>
 )
 
-const Total = ({ parts }) => {
-  const sum = parts.reduce((s, p) => s + p.exercises, 0)
-  return (
-    <p>
-      <strong>total of {sum} exercises</strong>
-    </p>
-  )
-}
+
 
 const Course = ({ course }) => (
   <div>
