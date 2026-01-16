51690e84d8a4871169fdd9a66b59056c6cdbc0d2
Karol Domański <karoltdom@wp.pl>
2026-01-16 13:08:34 +0100

    commit

---
+++ part2/2.7/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+
+    const nameExists = persons.some(person => person.name === newName)
+
+    if (nameExists) {
+      alert(`user ` + newName + ` already exists in the phonebook`)
+      return
+    }
+
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: 
+          <input 
+            value={newName} 
+            onChange={(e) => setNewName(e.target.value)} 
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      
+      <div>debug: {newName}</div>
+      
+      <h2>Numbers</h2>
+      
+      <ul>
+        {persons.map(person => 
+          <li key={person.name}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
