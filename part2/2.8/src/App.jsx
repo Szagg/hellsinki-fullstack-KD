d228e46564cb95b96958ee183ccc9b49ed3a9412
Karol Domański <karoltdom@wp.pl>
2026-01-16 13:22:03 +0100

    commit

---
+++ part2/2.8/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '123456789' }
+  ]) 
+
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+
+  const handleAddPerson = (event) => {
+    event.preventDefault()
+
+    const nameExists = persons.some(person => person.name === newName)
+    const phoneExists = persons.some(person => person.phone === newPhone)
+
+    if (nameExists) {
+      alert(`user ${newName} already exists in the phonebook`)
+      return
+    }
+    if (phoneExists) {
+      alert(`number ${newPhone} already exists in the phonebook`)
+      return
+    }
+
+    setPersons(persons.concat({ name: newName, phone: newPhone }))
+    setNewName('')
+    setNewPhone('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: 
+          <input 
+            value={newName} 
+            onChange={(e) => setNewName(e.target.value)} 
+          />
+        </div>
+        <div>
+          Phone Number: 
+          <input 
+            value={newPhone} 
+            onChange={(e) => setNewPhone(e.target.value)} 
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      
+      <div>debug: {newName} , {newPhone}</div>
+      
+      <h2>Numbers</h2>
+      
+      <ul>
+        {persons.map(person => 
+          <li key={person.name}>{person.name} - {person.phone}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
