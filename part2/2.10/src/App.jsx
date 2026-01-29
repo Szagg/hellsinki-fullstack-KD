11963476ef9d14df45ed36d5ec018cb14d49de46
Karol Domański <147516716+Szagg@users.noreply.github.com>
2026-01-29 12:35:17 +0100

    commit

--- part2/2.10/src/App.jsx
+++ part2/2.10/src/App.jsx
@@ -33,7 +33,7 @@ const Persons = ({ persons }) => (
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '12-34-56789' },
+    { name: 'Arto Hellas', phone: '12-34-56789' , id: 1},
     { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
