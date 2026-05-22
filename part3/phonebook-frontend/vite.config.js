9a9402a4695c86df978049dbdc45f51e31d0a600
Karol Domański <147516716+Szagg@users.noreply.github.com>
2026-05-22 12:07:57 +0200

    viteconfig fix

--- part3/phonebook-frontend/vite.config.js
+++ part3/phonebook-frontend/vite.config.js
@@ -1,1 +1,6 @@
-// ...copy your frontend vite.config.js here...
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
