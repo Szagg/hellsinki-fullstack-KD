cb6e1baf81fc6b40bc26461026b351f7863f097b
Karol Domański <karoltdom@wp.pl>
2026-05-29 12:35:46 +0200

    mongodb test

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -6,26 +6,41 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2]
+const url = `mongodb+srv://karoltdom:${password}@hellsinki.ansvmqq.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-const url = `mongodb+srv://karoltdom:<db_password>@hellsinki.ansvmqq.mongodb.net/?appName=Hellsinki`
-
-mongoose.set('strictQuery',false)
-
+mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
+if (process.argv.length === 3) {
+  // List all persons
+  Person.find({}).then(persons => {
+    console.log('phonebook:')
+    persons.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 5) {
+  // Add a new person
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-note.save().then(result => {
-  console.log('note saved!')
+  const person = new Person({ name, number })
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('usage:')
+  console.log('  node mongo.js <password>             # list all')
+  console.log('  node mongo.js <password> <name> <number>  # add new')
   mongoose.connection.close()
-})
+}
